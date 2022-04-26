271a5fbb0a34c2e675e45faf0e5e5a862c6896a0
danibex <danielivam96@gmail.com>
2022-04-26 01:29:37 -0300

    Renovação do projeto

--- script.js
+++ script.js
@@ -1,30 +1,10 @@
-function somar() {
-    let tn1 = document.getElementById('sn1')
-    let tn2 = document.getElementById('sn2')
-    let a = document.getElementById('resposta-soma')
-    let n1 = Number(tn1.value)
-    let n2 = Number(tn2.value)
-    s = n1 + n2
-    a.innerHTML = `O resultado da soma é: ${s}`
+let botoes = [] 
+
+function calcular() {
+
+for(let c = 0; c < 10; c++){
+    botoes[c] = Number(document.getElementsByClassName(c).value)
+    window.alert(botoes)
 }
 
-function subtrair() {
-    let subt1 = document.getElementById('sub1')
-    let subt2 = document.getElementById('sub2')
-    let texto = document.getElementById('resposta-subtracao')
-    let n1 = Number(subt1.value)
-    let n2 = Number(subt2.value)
-    s = n1 - n2
-    texto.innerHTML = `O resultado da subtração é: <strong>${s}</strong>`
-     
-}
-
-function multiplicar () {
-    let mu1 = document.getElementById('m1')
-    let mu2 = document.getElementById('m2')
-    let n1 = Number(mu1.value)
-    let n2 = Number(mu2.value)
-    let texto = document.getElementById('resposta-multiplicacao')
-    s = n1 * n2
-    texto.innerHTML = `O resultado da multiplicação é: ${s}`
 }
