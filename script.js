10a103d91abe559a4691c64962ba1cd94a28b77c
danibex <danielivam96@gmail.com>
2022-04-26 22:33:03 -0300

    adicionando js

--- script.js
+++ script.js
@@ -1,10 +1,39 @@
-let botoes = [] 
+let tela = document.getElementById("resultado")
 
-function calcular() {
 
-for(let c = 0; c < 10; c++){
-    botoes[c] = Number(document.getElementsByClassName(c).value)
-    window.alert(botoes)
+document.addEventListener("keydown", function(event) {
+    if(event.key >= 0 || event.key <= 9 || event.key === "+" || event.key === "-" || event.key === "Backspace"){
+        if(event.key === "Backspace") {
+            tela.innerHTML = ``
+        } else {
+            tela.innerHTML += event.key
+        }
+    }else {
+        window.alert(`[ERRO] "${event.key.toLocaleUpperCase()}" não é um número. Digite apenas números ou operações matemáticas!!!`)
+    }
+})
+
+
+function botao(numero) {
+    tela.innerHTML += numero
 }
 
+function soma() {
+    tela.innerHTML += `+`
 }
+function limparTela() {
+    tela.innerHTML = ``
+}
+
+function resposta() {
+    let operacao = document.getElementById("resultado")
+    alert(operacao.toString())
+}
+
+
+/*  document.addEventListener("keydown", function(event) {
+    window.alert(`Apertou o ${event.key}`)
+    console.log(event.key)
+})  */   
+
+// Como mapear teclas
